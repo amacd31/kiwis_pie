8f82835501687fd266eb1ab9c301adf915dabc12
Andrew MacDonald <andrew@maccas.net>
2016-07-19 19:07:41 +1000

    Dynamically add methods on import
    Instead of adding the dynamically generated class methods during object
    instantiation the methods are added to the class during import of
    kiwis_pie.kiwis. This will allow the methods to be inspected on the
    class without requiring an object instance.

--- kiwis_pie/kiwis.py
+++ kiwis_pie/kiwis.py
@@ -29,159 +29,10 @@ class KIWIS(object):
             'format': 'json',
         }
 
-        def parse_date(input_dt):
-            return pd.datetools.to_datetime(input_dt).strftime('%Y-%m-%d')
+def __parse_date(input_dt):
+    return pd.datetools.to_datetime(input_dt).strftime('%Y-%m-%d')
 
-        gen_kiwis_method(
-            self.__class__,
-            'getTimeseriesList',
-            {
-                'station_no': QueryOption(True, True, None),
-                'station_id': QueryOption(False, True, None),
-                'station_name': QueryOption(True, True, None),
-                'ts_id': QueryOption(False, True, None),
-                'ts_path': QueryOption(True, True, None),
-                'ts_name': QueryOption(True, True, None),
-                'ts_shortname': QueryOption(True, True, None),
-                'ts_type_id': QueryOption(False, True, None),
-                'parametertype_id': QueryOption(False, True, None),
-                'parametertype_name': QueryOption(True, True, None),
-                'stationparameter_name': QueryOption(True, True, None),
-                'stationparameter_no': QueryOption(False, True, None),
-                'ts_unitname': QueryOption(True, True, None),
-                'timeseriesgroup_id': QueryOption(False, False, None),
-                'fulltext': QueryOption(True, False, None),
-            },
-            [
-                'station_no',
-                'station_id',
-                'station_name',
-                'station_latitude',
-                'station_longitude',
-                'station_carteasting',
-                'station_cartnorthing',
-                'station_georefsystem',
-                'station_longname',
-                'ts_id',
-                'ts_name',
-                'ts_shortname',
-                'ts_pat',
-                'parametertype_id',
-                'parametertype_name',
-                'stationparameter_name',
-                'stationparameter_longname',
-                'ts_unitname',
-                'ts_unitsymbol',
-                'ts_unitname_abs',
-                'ts_unitsymbol_abs',
-                'coverage',
-                'ts_density',
-                'datacart',
-            ]
-        )
-
-        gen_kiwis_method(
-            self.__class__,
-            'getTimeseriesValues',
-            {
-                'ts_id': QueryOption(False, True, None),
-                'timeseriesgroup_id': QueryOption(False, True, None),
-                'ts_path': QueryOption(True, True, None),
-                'from': QueryOption(None, None, parse_date),
-                'to': QueryOption(None, None, parse_date),
-                'period': QueryOption(None, None, None),
-            },
-            [
-                'Timestamp',
-                'Value',
-                'Interpolation Type',
-                'Quality Code',
-                'Aggregation',
-                'Accuracy',
-                'Absolute Value',
-                'AV Interpolation',
-                'Type',
-                'AV Quality Code',
-                'Runoff Value',
-                'RV Interpolation',
-                'Type',
-                'RV Quality Code',
-            ]
-        )
-
-        gen_kiwis_method(
-            self.__class__,
-            'getStationList',
-            {
-                'station_no': QueryOption(True, True, None),
-                'station_id': QueryOption(False, True, None),
-                'station_name': QueryOption(True, True, None),
-                'catchment_no': QueryOption(False, True, None),
-                'catchment_id': QueryOption(False, True, None),
-                'catchment_name': QueryOption(True, True, None),
-                'site_no': QueryOption(False, True, None),
-                'site_id': QueryOption(False, True, None),
-                'site_name': QueryOption(False, True, None),
-                'stationgroup_id': QueryOption(False, False, None),
-                'parametertype_id': QueryOption(False, True, None),
-                'parametertype_name': QueryOption(True, True, None),
-                'stationparameter_name': QueryOption(True, True, None),
-            },
-            [
-                'station_no',
-                'station_id',
-                'station_name',
-                'catchment_no',
-                'catchment_id',
-                'catchment_name',
-                'station_latitude',
-                'station_longitude',
-                'station_carteasting',
-                'station_cartnorthing',
-                'site_no',
-                'site_id',
-                'site_name',
-                'parametertype_id',
-                'parametertype_name',
-                'stationparameter_name',
-                'object_type',
-                'station_georefsystem',
-                'station_longname',
-                'custom_attributes',
-            ]
-        )
-
-        gen_kiwis_method(
-            self.__class__,
-            'getSiteList',
-            {
-                'site_no': QueryOption(False, True, None),
-                'site_id': QueryOption(False, True, None),
-                'site_name': QueryOption(False, True, None),
-                'parametertype_id': QueryOption(False, True, None),
-                'parametertype_name': QueryOption(True, True, None),
-                'stationparameter_name': QueryOption(True, True, None),
-                'bbox': QueryOption(None, None, None),
-            },
-            [
-                'site_no',
-                'site_id',
-                'site_name',
-                'site_latitude',
-                'site_longitude',
-                'site_carteasting',
-                'site_cartnorthing',
-                'site_type_name',
-                'site_type_shortname',
-                'parametertype_id',
-                'parametertype_name',
-                'stationparameter_name',
-                'site_georefsystem',
-                'custom_attributes',
-            ]
-        )
-
-def gen_kiwis_method(cls, method_name, available_query_options, available_return_fields):
+def __gen_kiwis_method(cls, method_name, available_query_options, available_return_fields):
 
     start_snake = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', method_name)
     snake_name = re.sub('([a-z0-9])([A-Z])', r'\1_\2', start_snake).lower()
@@ -262,3 +113,152 @@ def gen_kiwis_method(cls, method_name, available_query_options, available_return
     kiwis_method.__doc__ = "{doc_intro}\n\n{query_option_table}\n\n{return_fields}".format(**docstring)
 
     setattr(cls, snake_name, kiwis_method)
+
+__gen_kiwis_method(
+    KIWIS,
+    'getTimeseriesList',
+    {
+        'station_no': QueryOption(True, True, None),
+        'station_id': QueryOption(False, True, None),
+        'station_name': QueryOption(True, True, None),
+        'ts_id': QueryOption(False, True, None),
+        'ts_path': QueryOption(True, True, None),
+        'ts_name': QueryOption(True, True, None),
+        'ts_shortname': QueryOption(True, True, None),
+        'ts_type_id': QueryOption(False, True, None),
+        'parametertype_id': QueryOption(False, True, None),
+        'parametertype_name': QueryOption(True, True, None),
+        'stationparameter_name': QueryOption(True, True, None),
+        'stationparameter_no': QueryOption(False, True, None),
+        'ts_unitname': QueryOption(True, True, None),
+        'timeseriesgroup_id': QueryOption(False, False, None),
+        'fulltext': QueryOption(True, False, None),
+    },
+    [
+        'station_no',
+        'station_id',
+        'station_name',
+        'station_latitude',
+        'station_longitude',
+        'station_carteasting',
+        'station_cartnorthing',
+        'station_georefsystem',
+        'station_longname',
+        'ts_id',
+        'ts_name',
+        'ts_shortname',
+        'ts_pat',
+        'parametertype_id',
+        'parametertype_name',
+        'stationparameter_name',
+        'stationparameter_longname',
+        'ts_unitname',
+        'ts_unitsymbol',
+        'ts_unitname_abs',
+        'ts_unitsymbol_abs',
+        'coverage',
+        'ts_density',
+        'datacart',
+    ]
+)
+
+__gen_kiwis_method(
+    KIWIS,
+    'getTimeseriesValues',
+    {
+        'ts_id': QueryOption(False, True, None),
+        'timeseriesgroup_id': QueryOption(False, True, None),
+        'ts_path': QueryOption(True, True, None),
+        'from': QueryOption(None, None, __parse_date),
+        'to': QueryOption(None, None, __parse_date),
+        'period': QueryOption(None, None, None),
+    },
+    [
+        'Timestamp',
+        'Value',
+        'Interpolation Type',
+        'Quality Code',
+        'Aggregation',
+        'Accuracy',
+        'Absolute Value',
+        'AV Interpolation',
+        'Type',
+        'AV Quality Code',
+        'Runoff Value',
+        'RV Interpolation',
+        'Type',
+        'RV Quality Code',
+    ]
+)
+
+__gen_kiwis_method(
+    KIWIS,
+    'getStationList',
+    {
+        'station_no': QueryOption(True, True, None),
+        'station_id': QueryOption(False, True, None),
+        'station_name': QueryOption(True, True, None),
+        'catchment_no': QueryOption(False, True, None),
+        'catchment_id': QueryOption(False, True, None),
+        'catchment_name': QueryOption(True, True, None),
+        'site_no': QueryOption(False, True, None),
+        'site_id': QueryOption(False, True, None),
+        'site_name': QueryOption(False, True, None),
+        'stationgroup_id': QueryOption(False, False, None),
+        'parametertype_id': QueryOption(False, True, None),
+        'parametertype_name': QueryOption(True, True, None),
+        'stationparameter_name': QueryOption(True, True, None),
+    },
+    [
+        'station_no',
+        'station_id',
+        'station_name',
+        'catchment_no',
+        'catchment_id',
+        'catchment_name',
+        'station_latitude',
+        'station_longitude',
+        'station_carteasting',
+        'station_cartnorthing',
+        'site_no',
+        'site_id',
+        'site_name',
+        'parametertype_id',
+        'parametertype_name',
+        'stationparameter_name',
+        'object_type',
+        'station_georefsystem',
+        'station_longname',
+        'custom_attributes',
+    ]
+)
+
+__gen_kiwis_method(
+    KIWIS,
+    'getSiteList',
+    {
+        'site_no': QueryOption(False, True, None),
+        'site_id': QueryOption(False, True, None),
+        'site_name': QueryOption(False, True, None),
+        'parametertype_id': QueryOption(False, True, None),
+        'parametertype_name': QueryOption(True, True, None),
+        'stationparameter_name': QueryOption(True, True, None),
+        'bbox': QueryOption(None, None, None),
+    },
+    [
+        'site_no',
+        'site_id',
+        'site_name',
+        'site_latitude',
+        'site_longitude',
+        'site_carteasting',
+        'site_cartnorthing',
+        'site_type_name',
+        'site_type_shortname',
+        'parametertype_id',
+        'parametertype_name',
+        'stationparameter_name',
+        'site_georefsystem',
+        'custom_attributes',
+    ]
+)
